228f093bc5a5fc6d4690df557a125a923d5b96bf
Sigurd Riseth <sigurd.riseth@gmail.com>
2024-10-24 15:20:01 +0200

    Kjørt Reformat Code før start.

--- src/main/java/no/nav/grunnbeløp/GrunnbeløpAPI.java
+++ src/main/java/no/nav/grunnbeløp/GrunnbeløpAPI.java
@@ -1,13 +1,12 @@
 package no.nav.grunnbeløp;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import org.json.JSONObject;
 
 /**
  * Har ansvaret for å kontakte grunnbeløp API'et til NAV og henter dagens grunnbeløp.
@@ -17,27 +16,30 @@ import java.net.http.HttpResponse;
  */
 public class GrunnbeløpAPI {
 
-    private final static Dotenv DOTENV = Dotenv.load();
+  private final static Dotenv DOTENV = Dotenv.load();
 
-    private final HttpClient grunnbeløpHTTPKlient;
+  private final HttpClient grunnbeløpHTTPKlient;
 
-    public GrunnbeløpAPI() {
-        this.grunnbeløpHTTPKlient = HttpClient.newHttpClient();
-    }
+  public GrunnbeløpAPI() {
+    this.grunnbeløpHTTPKlient = HttpClient.newHttpClient();
+  }
 
-    /**
-     * Kontakter grunnbeløp API'et til NAV og henter dagens grunnbeløp.
-     * Metoden sender en HTTP request og konverterer HTTP responsen til et <code>JSONObject</code>,
-     * som så henter dagens grunnbeløp fra JSON objektet.
-     * @return dagens grunnbeløp.
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    public double hentGrunnbeløp() throws IOException, InterruptedException {
-        HttpRequest grunnbeløpSpørring = HttpRequest.newBuilder(URI.create(DOTENV.get("G_API_URL"))).build();
+  /**
+   * Kontakter grunnbeløp API'et til NAV og henter dagens grunnbeløp. Metoden sender en HTTP request
+   * og konverterer HTTP responsen til et <code>JSONObject</code>, som så henter dagens grunnbeløp
+   * fra JSON objektet.
+   *
+   * @return dagens grunnbeløp.
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  public double hentGrunnbeløp() throws IOException, InterruptedException {
+    HttpRequest grunnbeløpSpørring = HttpRequest.newBuilder(URI.create(DOTENV.get("G_API_URL")))
+        .build();
 
-        HttpResponse<String> grunnbeløpRespons =  this.grunnbeløpHTTPKlient.send(grunnbeløpSpørring, HttpResponse.BodyHandlers.ofString());
+    HttpResponse<String> grunnbeløpRespons = this.grunnbeløpHTTPKlient.send(grunnbeløpSpørring,
+        HttpResponse.BodyHandlers.ofString());
 
-        return new JSONObject(grunnbeløpRespons.body()).getDouble("grunnbeløp");
-    }
+    return new JSONObject(grunnbeløpRespons.body()).getDouble("grunnbeløp");
+  }
 }
